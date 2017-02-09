4b783faaa60eed32184274b05aaabbe523c85f7e
nadejdanicolova <h0pe_@abv.bg>
2017-02-09 19:33:36 +0200

    grid view for top 10 series

---
+++ TvShowCalendar/app/src/main/java/com/telerikproject/tvshowcalendar/Adapter/GridAdapter.java
@@ -0,0 +1,85 @@
+package com.telerikproject.tvshowcalendar.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.telerikproject.tvshowcalendar.R;
+import com.telerikproject.tvshowcalendar.activities.MainActivity;
+
+/**
+ * Created by Admin68 on 9.2.2017 г..
+ */
+
+public class GridAdapter extends BaseAdapter {
+
+    String[] titles;
+    String[] series;
+    Context context;
+    int[] imageId;
+
+    private static LayoutInflater inflater = null;
+
+    public GridAdapter(MainActivity mainActivity, String[] moviesTitles, String[] moviesSeasons, int[] moviesImages) {
+        titles = moviesTitles;
+        series = moviesSeasons;
+        context = mainActivity;
+        imageId = moviesImages;
+
+        inflater = (LayoutInflater) context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public class Holder {
+        TextView titles;
+        TextView series;
+        ImageView images;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Holder holder = new Holder();
+        View rowView;
+
+        rowView = inflater.inflate(R.layout.grid_list, null);
+        holder.titles = (TextView) rowView.findViewById(R.id.tv_title);
+        holder.images = (ImageView) rowView.findViewById(R.id.iv_movie_image);
+        holder.series = (TextView) rowView.findViewById(R.id.tv_seasons);
+
+
+        holder.titles.setText(titles[position]);
+        holder.images.setImageResource(imageId[position]);
+        holder.series.setText(series[position]);
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+                Toast.makeText(context, "You Clicked " + titles[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rowView;
+    }
+}
--- TvShowCalendar/app/src/main/java/com/telerikproject/tvshowcalendar/activities/MainActivity.java
+++ TvShowCalendar/app/src/main/java/com/telerikproject/tvshowcalendar/activities/MainActivity.java
@@ -1,31 +1,44 @@
 package com.telerikproject.tvshowcalendar.activities;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.GridView;
 
+import com.telerikproject.tvshowcalendar.Adapter.GridAdapter;
 import com.telerikproject.tvshowcalendar.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    GridView gridView;
+    Context context;
+
+    public static int[] moviesImages = {R.drawable.the_originals, R.drawable.game_of_thrones,
+                                        R.drawable.mr_robot, R.drawable.the_big_bang_theory,
+                                        R.drawable.the_vampire_diaries, R.drawable.un_barco_en_el_espejo};
+    public static String[] moviesTitles = {"The Originals", "Game Of Thrones", "Mr. Robot", "The Big Bang Theory", "The Vampire Diaries", "El Barco"};
+    public static String[] moviesSeasons = {"4 seasons", "7 seasons", "3 seasons", "10 seasons", "8 seasons", "3 seasons"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        gridView = (GridView) findViewById(R.id.gv_top_10);
+        gridView.setAdapter(new GridAdapter(this, moviesTitles, moviesSeasons, moviesImages));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 }
